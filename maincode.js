b8d0804e2990d7b95b3e2969f11c2033e398b877
Hazel-XD <dixonhazel@rocketmail.com>
2020-05-16 23:56:34 +0100

    content updates

--- maincode.js
+++ maincode.js
@@ -1,4 +1,4 @@
-let files = ["crewroster", "navigation", "nel13", "missionobjective", "emailcorrespondence", "A"];
+let files = ["crewroster", "navigation", "nel13", "missionobjective", "emailcorrespondence", "emergency", "treadwell", "geological", "topsecret"];
 let t; //Putting "t" out here makes it a global variable so its accessible everywhere
 
 //Ignore this, I made it to make it easier for you to make inputs tha actually wait for the user to respond
@@ -123,6 +123,56 @@ async function commandEntered(userInput) {
         t.print("You were contacted by Captain Alison Starkey of the Royal Navy. Due to your expertise,  it  is believed that you possess the proficiency to rescue crew and equipment that has become stranded at this location."),
         t.print("..."),
         t.print("Your goal is to rescue the crew of the HMS Scylla and transport them to safety. ");
+        case "treadwell":
+        t.print("New Partnership Seeks to Shake Up the World of Deep Sea Mining and Exploration"),
+        t.print("Ellis T Coran"),
+        t.print("..."),
+        t.print("Lamprey Technologies and Treadwell today announced a partnership between their companies that is sure to reinvent the marine mining industry as we know it."),
+        t.print("..."),
+        t.print("For a number of years now, Lamprey have held a sturdy grip on their position as industry leaders, a position that has been threatened by a number of ambitious startup companies such as thow now infamous Deepest B.V."),
+        t.print("..."),
+        t.print("Treadwell, a corporation with links to an eclectic mix of investments in chemical and mechanical engineering, pharmacology and computer part production, now seem to be branching out even further to partner with Lamprey in their latest venture."),
+        t.print("..."),
+        t.print("Nobody is sure what to expect from these two heavyweights of privately funded research world, but it’s sure to be explosive - and both companies have seen share prices skyrocket since the announcement of their collaboration."),
+        t.print("..."),
+        t.print("More on this exciting story as details develop...");
+        break;
+        case "emergency":
+        t.print("Emergency Guidelines: Trafalgar-Class Submarine"),
+        t.print("..."),
+        t.print("In case of an emergency while surfaced, radio communication with control or any viable alternative should be established and a distress message sent. If not surfaced, but within range of the buoy communication system, the buoy with the most appropriate antenna (either for surface radio or VLF communication) should be deployed."),
+        t.print("..."),
+        t.print("Whilst submersed, the priority should be to safely surface. In a situation where this cannot happen, do not take unnecessary risks. Either remain in place and focus on keeping crew together and safe for as long as possible whilst waiting for rescue, or exit the vessel using the Submarine Escape Immersion Equipment (SEIE)."),
+        t.print("..."),
+        t.print("All Trafalgar-Class Submarines are equipped with at least one set of SEIE, allowing one crew member to abort and radio for help once surfaced. Lamprey Technologies ensures, in addition to this, that where at all possible vessels travel with enough sets of SEIE for every member of crew."),
+        t.print("..."),
+        t.print("In the case of a power outage, the submarine contains oxygen reserves to last at least 20 hours for a standard crew. Emergency blankets found in crew quarters can be used to combat hypothermia resulting from a non-operational heating system. In case of issues with the water system, filtration tablets can be found in the crew mess."),
+        t.print("..."),
+        t.print("In a worst case scenario, the crew should gather together in one room and focus on only supplying that location with oxygen. Any fires should be extinguished as quickly as possible to save air, and any power supplies should be focused on pumping seawater away from this location.");
+        break;
+        case "topsecret":
+        t.print("asldjf3oj2o;34jto4hi43htqit43qijth4;iq34uhq;akjshdfjawe;ht4qtuq4tuha;sdncqo4uhta;w4ohta;wo4thao;sdhgfa;4329438adouhfa;w4uht2948tya;suohfas");
+        break;
+        case "geological":
+        t.print("Report on North Atlantic Ocean anomaly site: 57°13'33.7'N 33°25'42.8'W')"),
+        t.print("..."),
+        t.print("In recent months there has been increased seismic activity along this fault line, as well as a spike in sightings of deep sea flora and fauna."),
+        t.print("..."),
+        t.print("Analysis of sheer and seismic waves produced by the microearthquakes around the fault have revealed a solid mass of metallic substance beneath the tectonic plate. This mass, which lacks the familiar composition of a mantle plume, has never been observed before here or elsewhere, and seems to extend further than the observed waves could be mapped. We believe, therefore, that it indicates the presence of a vein of rare metal, exposed for the first time due to this increase in seismic activity."),
+        t.print("..."),
+        t.print("Following preliminary investigations of a small sample of the metal extracted by NEL-13, we have observed the following qualities:"),
+        t.print("This metal is not one previously observed by geologists"),
+        t.print("Phase at STP: viscous solid"),
+        t.print("Crystal structure: face-centred cubic"),
+        t.print("Highly magnetic"),
+        t.print("Interferes with electromagnetic radiation, especially at low frequencies"),
+        t.print("..."),
+        t.print("As scientific investigation and mining have started operating in the area, the seismic activity seems to have further increased in both frequency and intensity. There is no logical reason why this would be the case beyond the expected levels of microearthquakes from methane released by drilling into the seafloor. However, the intensity of quakes being recorded is exponentially higher than what we would predict for the level of mining activity."),
+        t.print("..."),
+        t.print("We can supply no reason for this, and will investigate further. Until conclusion can be drawn, we recommend reducing the amount of mining activity in the area."),
+        t.print("..."),
+        t.print("Whilst this report raises some valid concerns, no imminent danger is flagged and so mining is cleared to continue at the current level, with increased production once new sites have been mapped and proper equipment installed -- Matthias Hoffman");
+        break;
         case "quit":
             t.print("Quit. Please reload terminal to continue");
             running = false;
